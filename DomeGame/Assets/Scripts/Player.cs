817bb3e352c0795366a52d737f61f341a2b8ad8e
Kenneth Lee <kl488@cornell.edu>
2025-01-25 17:59:58 -0600

    restart game on space when gameover

--- DomeGame/Assets/Scripts/Player.cs
+++ DomeGame/Assets/Scripts/Player.cs
@@ -180,6 +180,9 @@ public class Player : MonoBehaviour
                 ImmediateStyle.Text("/Canvas/EndText82ef", "GameOver");
 
             }
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
             return;
         }
         if (currentPhase == GamePhase.StartPhase) {
