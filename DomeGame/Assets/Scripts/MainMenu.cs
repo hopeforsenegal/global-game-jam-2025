943458c9b206945a01a8bfe75cd88aad2fa89d6f
Kamau Vassall <kamau.vassall@gmail.com>
2025-01-24 21:45:04 -0600

    Adding in fake main menu

---
+++ DomeGame/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+using MoonlitSystem.UI.Immediate;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    protected void Update()
+    {
+        ImmediateStyle.CanvasGroup("/Canvas/MainMenua071");
+        if (ImmediateStyle.Button("/Canvas/Image/Button (Legacy)c95e").IsMouseDown) {
+
+        }
+        if (ImmediateStyle.Button("/Canvas/Image/Button (Legacy) (1)1eee").IsMouseDown) {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+    }
+}
