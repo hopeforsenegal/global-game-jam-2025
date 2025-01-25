5c66315eed0720418e1b105a9a443f887781780f
Kenneth Lee <kl488@cornell.edu>
2025-01-25 13:22:52 -0600

    add in resources

--- DomeGame/Assets/Scripts/Player.cs
+++ DomeGame/Assets/Scripts/Player.cs
@@ -1,12 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using MoonlitSystem.UI.Immediate;
+using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public int population = 1;
-    public int unitFood = 10;
+
+    public int currentTurn = 0;
+    public int maxTurns = 5;
+    public int currentCitizenPopulation = 100;
+    public int currentFood = 1000;
+    public int currentUranium = 100;
+    public int currentWater = 0;
+
+    public int rateFoodPerCitizen = 1;
+    public int rateUraniumPerCitizen = 1;
+    public int rateWaterPerCitizen = 1;
+    public int rateWaterPerUranium = 1;
+
+    public int surplusFoodToGrowOneCitizenPerTurn = 10;
+
+    public int rateCitizenDeathPerNoFood = 1;
+
 
 
     // Start is called before the first frame update
@@ -15,10 +31,51 @@ public class Player : MonoBehaviour
         
     }
 
+    int citizenGrowthPerTurn(){
+        if (currentFood >= currentCitizenPopulation) {
+            return Math.Max(currentFood-currentCitizenPopulation,0)/surplusFoodToGrowOneCitizenPerTurn;
+        }
+        return (currentFood - currentCitizenPopulation) * rateCitizenDeathPerNoFood;
+    }
+
+    int foodGrowthPerTurn(){
+        return -rateFoodPerCitizen*currentCitizenPopulation;
+    }
+
+    int uraniumGrowthPerTurn(){
+        return -rateUraniumPerCitizen*currentCitizenPopulation;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        ImmediateStyle.Text("/Canvas/FoodTexte6fc", "Food: " + unitFood);
-        ImmediateStyle.Text("/Canvas/FoodText (1)4f43", "Population: " + population);
+        ImmediateStyle.Text("/Canvas/FoodTexte6fc", "Food:" + currentFood + "(" + foodGrowthPerTurn() + "/turn)");
+        ImmediateStyle.Text("/Canvas/FoodText (1)4f43", "Population:" + currentCitizenPopulation + "(" + citizenGrowthPerTurn() + "/turn)");
+        ImmediateStyle.Text("/Canvas/UraniumTextc4b3", "Uranium:" + currentUranium + "(" + uraniumGrowthPerTurn() + "/turn)");
+        ImmediateStyle.Text("/Canvas/TurnText366e", "Turn:" + currentTurn);
+
+    
+        if (currentTurn == maxTurns) {
+            if (currentCitizenPopulation > 0) {
+                ImmediateStyle.Text("/Canvas/EndText82ef", "You Win");
+
+            } else {
+                ImmediateStyle.Text("/Canvas/EndText82ef", "GameOver");
+
+            }
+            return;
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            int citizenGrowth = citizenGrowthPerTurn();
+            int foodGrowth = foodGrowthPerTurn();
+            int uraniumGrowth = uraniumGrowthPerTurn();
+            currentCitizenPopulation = math.max(citizenGrowth + currentCitizenPopulation, 0);
+            currentFood = math.max(foodGrowth + currentFood, 0);
+            currentUranium = math.max(uraniumGrowth + currentUranium, 0);
+
+            currentTurn = currentTurn + 1;
+        }
     }
 }
