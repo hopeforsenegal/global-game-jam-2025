a507cf1a6679966b1ea89da3f33e58d1a6f7d1d8
Kenneth Lee <kl488@cornell.edu>
2025-01-25 13:49:11 -0600

    add remaining getter/setter

--- DomeGame/Assets/Scripts/Player.cs
+++ DomeGame/Assets/Scripts/Player.cs
@@ -1,56 +1,81 @@
 using System;
 using MoonlitSystem.UI.Immediate;
 using Unity.Mathematics;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
 
-    public int currentTurn = 0;
-    public int maxTurns = 5;
-    public int currentCitizenPopulation = 100;
-    public int currentFood = 1000;
-    public int currentUranium = 100;
-    public int currentWater = 0;
+    [SerializeField] int currentTurn = 0;
+    [SerializeField] int maxTurns = 5;
+    [SerializeField] int currentCitizenPopulation = 100;
+    [SerializeField] int currentFood = 1000;
+    [SerializeField] int currentUranium = 100;
+    [SerializeField] int currentWater = 0;
+    [SerializeField] int currentCoin = 0;
 
-    public int rateFoodPerCitizen = 1;
-    public int rateUraniumPerCitizen = 1;
-    public int rateWaterPerCitizen = 1;
-    public int rateWaterPerUranium = 1;
+    [SerializeField] int rateFoodPerCitizen = 1;
+    [SerializeField] int rateUraniumPerCitizen = 1;
+    [SerializeField] int rateWaterPerCitizen = 1;
+    [SerializeField] int rateWaterPerUranium = 1;
+    [SerializeField] int rateCoinPerTurn = 0;
 
-    public int surplusFoodToGrowOneCitizenPerTurn = 10;
+    [SerializeField] int foodGeneratedPerAssignedCitizen = 100;
+    [SerializeField] int coinGeneratedPerAssignedCitizen = 100;
+    [SerializeField] int waterGeneratedPerAssignedCitizen = 100;
 
-    public int rateCitizenDeathPerNoFood = 1;
+    [SerializeField] int uraniumGeneratedPerAssignedCitizen = 100;
 
-    public int calculateRateFoodGivenCitizen(int citizen) {
-        return rateFoodPerCitizen * citizen;
+
+    [SerializeField] int surplusFoodToGrowOneCitizenPerTurn = 10;
+
+    [SerializeField] int rateCitizenDeathPerNoFood = 1;
+
+    public int CalculateRateFoodGivenCitizen(int citizen) {
+        return rateFoodPerCitizen * citizen + foodGrowthPerTurn();
     }
 
-    public int calculateRateUraniumGivenCitizen(int citizen) {
-        return rateUraniumPerCitizen * citizen;
+    public int CalculateRateUraniumGivenCitizen(int citizen) {
+        return rateUraniumPerCitizen * citizen + uraniumGrowthPerTurn();
     }
 
-    public int calculateRateWaterGivenCitizen(int citizen) {
-        return rateWaterPerCitizen * citizen;
+    public int CalculateRateWaterGivenCitizen(int citizen) {
+        return rateWaterPerCitizen * citizen + waterGrowthPerTurn();
     }
 
-    public int calculateRateCoinGivenCitizen(int citizen) {
-        return 0;
+    public int CalculateRateCoinGivenCitizen(int citizen) {
+        return citizen + coinGrowthPerTurn();
     }
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    int citizenGrowthPerTurn(){
+    public int CitizenGrowthPerTurn(){
         if (currentFood >= currentCitizenPopulation) {
             return Math.Max(currentFood-currentCitizenPopulation,0)/surplusFoodToGrowOneCitizenPerTurn;
         }
         return (currentFood - currentCitizenPopulation) * rateCitizenDeathPerNoFood;
+    }
+
+    public int GetCurrentCitizenPopulation(){
+        return currentCitizenPopulation;
+    }
+
+    public int GetCurrentFood(){
+        return currentFood;
+    }
+
+    public int GetCurrentWater(){
+        return currentWater;
+    }
+
+    public int GetCurrentUranium(){
+        return currentUranium;
+    }
+
+    public int GetCurrentCoin(){
+        return currentCoin;
+    }
+
+    public int GetCurrentTurn() {
+        return currentTurn;
     }
 
     int foodGrowthPerTurn(){
@@ -61,12 +86,22 @@ public class Player : MonoBehaviour
         return -rateUraniumPerCitizen*currentCitizenPopulation;
     }
 
+    int waterGrowthPerTurn(){
+        return -rateWaterPerCitizen*currentCitizenPopulation + -rateWaterPerUranium*currentUranium;
+    }
+
+    int coinGrowthPerTurn(){
+        return rateCoinPerTurn;
+    }
+
     // Update is called once per frame
     void Update()
     {
+        ImmediateStyle.Text("/Canvas/CoinText4142", "Coin: " + currentCoin + "(" + coinGrowthPerTurn() + "/turn)");
         ImmediateStyle.Text("/Canvas/FoodTexte6fc", "Food:" + currentFood + "(" + foodGrowthPerTurn() + "/turn)");
-        ImmediateStyle.Text("/Canvas/FoodText (1)4f43", "Population:" + currentCitizenPopulation + "(" + citizenGrowthPerTurn() + "/turn)");
+        ImmediateStyle.Text("/Canvas/FoodText (1)4f43", "Population:" + currentCitizenPopulation + "(" + CitizenGrowthPerTurn() + "/turn)");
         ImmediateStyle.Text("/Canvas/UraniumTextc4b3", "Uranium:" + currentUranium + "(" + uraniumGrowthPerTurn() + "/turn)");
+        ImmediateStyle.Text("/Canvas/WaterText798e", "Water:" + currentWater + "(" + waterGrowthPerTurn() + "/turn)");
         ImmediateStyle.Text("/Canvas/TurnText366e", "Turn:" + currentTurn);
 
     
@@ -83,13 +118,16 @@ public class Player : MonoBehaviour
 
 
         if (Input.GetKeyDown(KeyCode.Space)) {
-            int citizenGrowth = citizenGrowthPerTurn();
+            int citizenGrowth = CitizenGrowthPerTurn();
             int foodGrowth = foodGrowthPerTurn();
             int uraniumGrowth = uraniumGrowthPerTurn();
+            int waterGrowth = waterGrowthPerTurn();
+            int coinGrowth = coinGrowthPerTurn();
             currentCitizenPopulation = math.max(citizenGrowth + currentCitizenPopulation, 0);
             currentFood = math.max(foodGrowth + currentFood, 0);
             currentUranium = math.max(uraniumGrowth + currentUranium, 0);
-
+            currentWater = math.max(waterGrowth + currentWater, 0);
+            currentCoin = math.max(coinGrowth + currentCoin, 0);    
             currentTurn = currentTurn + 1;
         }
     }
