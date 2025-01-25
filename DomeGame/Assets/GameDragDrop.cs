efebce5c03bfc831909b4f5af39f8886a2f2c5d5
Kamau Vassall <kamau.vassall@gmail.com>
2025-01-25 00:59:28 -0600

    Show off some easy drag and drop in the UI

---
+++ DomeGame/Assets/GameDragDrop.cs
@@ -0,0 +1,22 @@
+using MoonlitSystem.UI.Immediate;
+using UnityEngine;
+
+public class GameDragDrop : MonoBehaviour
+{
+    void Update()
+    {
+        var hasDropped1 = ImmediateStyle.DragDrop("/Canvas/Bottom Bar/Population7855", out var component).IsMouseUp;
+
+        if (component.IsDragging) ImmediateStyle.FollowCursor(component.transform);
+
+        if (hasDropped1) {
+            var slot = Reference.Find<RectTransform>(this, "/Canvas/City/Image701f");
+
+            if (RectTransformUtility.RectangleContainsScreenPoint(slot, component.transform.position)) {
+                component.PinnedPosition = slot.position;
+            }
+
+            component.transform.position = component.PinnedPosition;
+        }
+    }
+}
