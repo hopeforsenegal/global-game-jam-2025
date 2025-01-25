39db947f7adbb10e192f58f3b0c1a776fa02a277
Kamau Vassall <kamau.vassall@gmail.com>
2025-01-25 01:01:50 -0600

    Make this a bit easier to follow

--- DomeGame/Assets/GameDragDrop.cs
+++ DomeGame/Assets/GameDragDrop.cs
@@ -3,13 +3,14 @@ using UnityEngine;
 
 public class GameDragDrop : MonoBehaviour
 {
-    void Update()
+    protected void Update()
     {
-        var hasDropped1 = ImmediateStyle.DragDrop("/Canvas/Bottom Bar/Population7855", out var component).IsMouseUp;
+        var hasDropped = ImmediateStyle.DragDrop("/Canvas/Bottom Bar/Population7855", out var component).IsMouseUp;
 
-        if (component.IsDragging) ImmediateStyle.FollowCursor(component.transform);
-
-        if (hasDropped1) {
+        if (component.IsDragging) {
+            ImmediateStyle.FollowCursor(component.transform);
+        }
+        if (hasDropped) {
             var slot = Reference.Find<RectTransform>(this, "/Canvas/City/Image701f");
 
             if (RectTransformUtility.RectangleContainsScreenPoint(slot, component.transform.position)) {
