ce02f04aac08e61acba3864838091fed3ded5241
Ed Solis <edsfocci@gmail.com>
2025-01-25 21:40:55 -0600

    RandomEvents: begin alignment of data structure to fit script

--- DomeGame/Assets/Data/Event.cs
+++ DomeGame/Assets/Data/Event.cs
@@ -2,15 +2,23 @@ using System;
 using UnityEngine;
 
 [Serializable]
+public class DialogPart
+{
+    public String dialogText;
+    public Choice[] choices;
+}
+
+[Serializable]
 public class Choice
 {
-    public String text;
+    public String choiceText;
+    public DialogPart[] dialogIfSelected;
+    // TODO: Extra credit
+    // public Choice[] subChoices;
 }
 
 [CreateAssetMenu(fileName = "Event", menuName = "Event", order = 1)]
 public class Event : ScriptableObject
 {
-    public Sprite sprite;
-    public String description;
-    public Choice[] choices;
+    public DialogPart[] dialog;
 }
--- DomeGame/Assets/Scripts/RandomEvents.cs
+++ DomeGame/Assets/Scripts/RandomEvents.cs
@@ -8,57 +8,76 @@ using UnityEngine.UI;
 
 public class RandomEvents : MonoBehaviour
 {
-    public Event[] eventsData;
+    public Event[] randomEventsData;
     private Event selectedEvent;
     private TypingEffect typingEffect;
+    private uint dialogIndex = 0;
+    private uint choiceDialogIndex;
 
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Start");
 
-        selectedEvent = eventsData[UnityEngine.Random.Range(0, eventsData.Length)];
+        selectedEvent = randomEventsData[UnityEngine.Random.Range(0, randomEventsData.Length)];
         typingEffect = new TypingEffect();
-        typingEffect.fullText = selectedEvent.description.Replace("\\n", System.Environment.NewLine);
+        typingEffect.fullText =
+            selectedEvent.dialog[dialogIndex].dialogText.Replace("\\n", System.Environment.NewLine);
     }
 
     // Update is called once per frame
     void Update()
     {
-        // if (Input.GetKeyDown(KeyCode.Space)) {
-        //     Debug.Log("Hit space");
-        // }
+        if (Input.GetKeyDown(KeyCode.Space) && selectedEvent.dialog[dialogIndex].choices.Length == 0) {
+            Debug.Log("Hit space");
+            dialogIndex++;
+
+            if (dialogIndex < selectedEvent.dialog.Length)
+            {
+                typingEffect.fullText =
+                    selectedEvent.dialog[dialogIndex].dialogText.Replace("\\n", System.Environment.NewLine);
+            }
+            else
+            {
+                #if UNITY_EDITOR
+                    UnityEditor.EditorApplication.isPlaying = false;
+                #else
+                    Application.Quit();
+                #endif
+            }
+        }
 
         TypingEffect.HandleTypingEffect(ref typingEffect, 0.1f);
-        // ImmediateStyle.Text("/Canvas/Event Description Background/Event Descriptiond52a", typingEffect.currentText);
-        ImmediateStyle.Text("/Canvas/Event Description Background/Event Descriptiond52a", selectedEvent.description.Replace("\\n", System.Environment.NewLine));
+        ImmediateStyle.Text("/Canvas/Event Description Background/Event Descriptiond52a", typingEffect.currentText);
 
-        if (selectedEvent.choices.Length > 1)
+        if (selectedEvent.dialog[dialogIndex].choices.Length > 1)
         {
-            ImmediateStyle.Image("/Canvas/Event Sprite5b8c", selectedEvent.sprite);
+            // TODO: After implementing more than 1 Event Sprite
+            // ImmediateStyle.Image("/Canvas/Event Sprite5b8c", selectedEvent.sprite);
 
-            ImmediateStyle.Text("/Canvas/Button A/Text A6222", selectedEvent.choices[0].text);
+            ImmediateStyle.Text("/Canvas/Button A/Text A6222", selectedEvent.dialog[dialogIndex].choices[0].choiceText);
             if (ImmediateStyle.Button("/Canvas/Button A3acd").IsMouseDown)
             {
                 Debug.Log("Button 1");
             }
-            ImmediateStyle.Text("/Canvas/Button B/Text Bf90d", selectedEvent.choices[1].text);
+
+            ImmediateStyle.Text("/Canvas/Button B/Text Bf90d", selectedEvent.dialog[dialogIndex].choices[1].choiceText);
             if (ImmediateStyle.Button("/Canvas/Button Bd2b9").IsMouseDown)
             {
                 Debug.Log("Button 2");
             }
 
-            if (selectedEvent.choices.Length > 2)
+            if (selectedEvent.dialog[dialogIndex].choices.Length > 2)
             {
-                ImmediateStyle.Text("/Canvas/Button C/Text Cc803", selectedEvent.choices[2].text);
+                ImmediateStyle.Text("/Canvas/Button C/Text Cc803", selectedEvent.dialog[dialogIndex].choices[2].choiceText);
                 if (ImmediateStyle.Button("/Canvas/Button C2345").IsMouseDown)
                 {
                     Debug.Log("Button 3");
                 }
 
-                if (selectedEvent.choices.Length == 4)
+                if (selectedEvent.dialog[dialogIndex].choices.Length == 4)
                 {
-                    ImmediateStyle.Text("/Canvas/Button D/Text D965b", selectedEvent.choices[3].text);
+                    ImmediateStyle.Text("/Canvas/Button D/Text D965b", selectedEvent.dialog[dialogIndex].choices[3].choiceText);
                     if (ImmediateStyle.Button("/Canvas/Button D3661").IsMouseDown)
                     {
                         Debug.Log("Button 4");
@@ -67,4 +86,28 @@ public class RandomEvents : MonoBehaviour
             }
         }
     }
+
+    // private void handleSelectedChoice(Choice selectedChoice)
+    // {
+    //     choiceDialogIndex = 0;
+
+    //     if (Input.GetKeyDown(KeyCode.Space) && selectedEvent.dialog[dialogIndex].choices.Length == 0) {
+    //         Debug.Log("Hit space");
+    //         dialogIndex++;
+
+    //         if (dialogIndex < selectedEvent.dialog.Length)
+    //         {
+    //             typingEffect.fullText =
+    //                 selectedEvent.dialog[dialogIndex].dialogText.Replace("\\n", System.Environment.NewLine);
+    //         }
+    //         else
+    //         {
+    //             #if UNITY_EDITOR
+    //                 UnityEditor.EditorApplication.isPlaying = false;
+    //             #else
+    //                 Application.Quit();
+    //             #endif
+    //         }
+    //     }
+    // }
 }
