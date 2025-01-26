d34b0e012f17ce2437ff858a5dc346234e3c7672
Kamau Vassall <kamau.vassall@gmail.com>
2025-01-26 14:53:09 -0600

    Made the intro 3 screens

--- DomeGame/Assets/Scripts/GameManager_Intro.cs
+++ DomeGame/Assets/Scripts/GameManager_Intro.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 
 public partial class GameManager
 {
+    public enum State { Intro1, Intro2, Intro3, }
     struct IntroState
     {
         public float fadeStartTime;
         public bool isFading;
+        public State state;
     }
     IntroState m_IntroState;
 
@@ -16,6 +18,7 @@ public partial class GameManager
         m_IntroState = new IntroState { };
         m_IntroState.fadeStartTime = Time.time - 0.3f;
         m_IntroState.isFading = true;
+        m_IntroState.state = State.Intro1;
     }
 
     private void HandleIntro()
@@ -25,8 +28,24 @@ public partial class GameManager
         if (!m_IntroState.isFading) ImmediateStyle.CanvasGroup("/Canvas (Environment)/Intro/Background (1)aeb8", (cg) => cg.alpha = 1);
         if (m_IntroState.isFading) ImmediateStyle.CanvasGroup("/Canvas (Environment)/Intro/Background (1)aeb8", (cg) => { cg.alpha = Mathf.Lerp(1, 0, Time.time - m_IntroState.fadeStartTime); });
 
+        if (m_IntroState.state == State.Intro2) ImmediateStyle.Image("/Canvas (Environment)/Intro/Mask/DialougeBox (1)/CitizenToken5587");
+
+        if (m_IntroState.state == State.Intro2) ImmediateStyle.CanvasGroup("/Canvas (Environment)/Intro/Intro39c8d");
+        if (m_IntroState.state == State.Intro3) ImmediateStyle.CanvasGroup("/Canvas (Environment)/Intro/Intro27b15");
+
+        var text = m_IntroState.state switch
+        {
+            State.Intro1 => gameSettings.intro1,
+            State.Intro2 => gameSettings.intro2,
+            State.Intro3 => gameSettings.intro3,
+        };
+        ImmediateStyle.Text("/Canvas (Environment)/Intro/Mask/DialougeBox (1)/Textaf0b", text);
+
         if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) {
-            Screen = GameScreens.Core;
+            m_IntroState.state++;
+            if (m_IntroState.state > State.Intro3) {
+                Screen = GameScreens.Core;
+            }
         }
     }
 }
