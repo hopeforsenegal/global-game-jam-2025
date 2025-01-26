624d627f4e69680ee365c728b966730acd78c0b7
Kamau Vassall <kamau.vassall@gmail.com>
2025-01-25 21:31:39 -0600

    joined core into the game manager

--- DomeGame/Assets/Scripts/GameManager.cs
+++ DomeGame/Assets/Scripts/GameManager.cs
@@ -5,6 +5,7 @@ public partial class GameManager : MonoBehaviour
 {
     void Start()
     {
+        StartRealCore();
         StartCore();
     }
 
@@ -19,12 +20,13 @@ public partial class GameManager : MonoBehaviour
     {
         switch (Screen) {
             case GameScreens.MainMenu: HandleMainMenu(); break;
-            case GameScreens.Core: HandleCore(); break;
+            case GameScreens.Core: HandleCore(); HandleRealCore(); break;
             case GameScreens.RandomEvents: HandleRandomEvents(); break;
             default: throw new ArgumentOutOfRangeException(Screen.ToString());
         }
 
         if (Input.GetKeyDown(KeyCode.Q)) { Debug.Log("Q"); Screen = GameScreens.MainMenu; m_MainMenuState = default; }
         if (Input.GetKeyDown(KeyCode.W)) { Debug.Log("W"); Screen = GameScreens.Core; StartCore(); }
+        if (Input.GetKeyDown(KeyCode.E)) { Debug.Log("E"); Screen = GameScreens.Core; StartRealCore(); }
     }
 }
--- DomeGame/Assets/Scripts/GameManager_MainMenu.cs
+++ DomeGame/Assets/Scripts/GameManager_MainMenu.cs
@@ -3,8 +3,6 @@ using UnityEngine;
 
 public partial class GameManager 
 {
-    public int heyyoucanseeme;
-
     struct MainMenuState
     {
         public float fadeStartTime;
